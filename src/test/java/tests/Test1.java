d83983ae0f1363edd5c04b3915053714dae23a5a
SunDRise <99346365+SunDRise@users.noreply.github.com>
2022-03-13 23:34:18 +0530

    Update Test1.java
    changed

--- src/test/java/tests/Test1.java
+++ src/test/java/tests/Test1.java
@@ -4,7 +4,7 @@ public class Test1
 {
 	public static void main(String[] args) 
 	{
-		String x="ufbukfhbk#$^&&57$^784HJ";
+		String x="ufbukfhbk#$^&&57$^784HJA";
 		String y=x.replaceAll("[^0-9]", "");
 		System.out.println(y);
 
